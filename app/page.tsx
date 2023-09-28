9567f36f31181ef8ed3f1f13644f5416c1e40de4
Trung Lam <trunglam@Trungs-MacBook-Pro.local>
2023-09-27 21:43:58 -0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+import { useState } from "react";
+import ConvexClientProvider from "./components/ConvexClientProvider";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
 
 export default function Home() {
+
+  const [taskName, setTaskName] = useState('');
+  const tasks = useQuery(api.tasks.get);
+  const createTask = useMutation(api.tasks.createTask);
+  const deleteTask = useMutation(api.tasks.deleteTask);
+  const updateTask = useMutation(api.tasks.updateTask);
+
+  const addTaskHandler = () => {
+    createTask({isCompleted: false, text: taskName});
+  }
+
+  const deleteTaskHandler = (taskId : any) => {
+    deleteTask({id: taskId});
+  }
+
+  const finishTaskHandler = (taskId : any) => {
+    updateTask({id: taskId})
+  }
+
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {tasks?.map(({ _id, text }) => (
+        <div key={_id}>
+          <div>{text}</div>
+          <button onClick={() => finishTaskHandler(_id)}>Finish</button>
+          <button onClick={() => deleteTaskHandler(_id)}>Delete</button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      ))}
+      <input placeholder="Task Name" onChange={e => setTaskName(e.target.value)}/>
+      <button onClick={addTaskHandler}>Add Task</button>
+      <div>
+        <LoginButton />
       </div>
     </main>
-  )
+  );
 }
