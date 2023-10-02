a16e8517913819b13a707a505c56d991a5df20e2
Trung Lam <trunglam@Trungs-MacBook-Pro.local>
2023-10-01 18:33:41 -0700

    fixed warning

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -9,6 +9,10 @@ interface LoginProps {
 
 export default function Login({authenticate} : LoginProps) {
 
+    const buttonSx = {
+        color: 'white'
+    };
+
     return (
         <div className={styles.login_container}>
             {/* <div className={styles.hero_container}>
@@ -23,7 +27,7 @@ export default function Login({authenticate} : LoginProps) {
             </div> */}
             <div className={styles.login_side}>
                 <h1>PupTop</h1>
-                <Button variant='contained' onClick={() => authenticate()}>Log In or Sign Up</Button>
+                <Button variant='contained' sx={buttonSx} onClick={() => authenticate()}>Log In or Sign Up</Button>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,22 @@ import LoginButton from "./components/LoginButton";
 import styles from "./page.module.css"
 import Header from "./components/Header";
 import Login from "./components/Login";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 
 export default function Home() {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   const [taskName, setTaskName] = useState('');
+
+  const mainTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#009eff',
+      }, 
+    }
+  });
+
   const tasks = useQuery(api.tasks.get);
   const createTask = useMutation(api.tasks.createTask);
   const deleteTask = useMutation(api.tasks.deleteTask);
@@ -40,25 +50,28 @@ export default function Home() {
   }
   
   return (
-    isAuthenticated ? 
-    <>
-      <Header logout={logoutHandler}/>
-      <main>
-        {/* {tasks?.map(({ _id, text }) => (
-          <div key={_id}>
-            <div>{text}</div>
-            <button onClick={() => finishTaskHandler(_id)}>Finish</button>
-            <button onClick={() => deleteTaskHandler(_id)}>Delete</button>
-          </div>
-        ))}
-        <input placeholder="Task Name" onChange={e => setTaskName(e.target.value)}/>
-        <button onClick={addTaskHandler}>Add Task</button>
-        <div>
-          <LoginButton />
-        </div> */}
-      </main>
-    </> :
-    <Login authenticate={authenticateUser}/>
+    <ThemeProvider theme={mainTheme}>
+      {isAuthenticated ? 
+        <>
+          <Header logout={logoutHandler}/>
+          <main className={styles.main}>
+            {/* {tasks?.map(({ _id, text }) => (
+              <div key={_id}>
+                <div>{text}</div>
+                <button onClick={() => finishTaskHandler(_id)}>Finish</button>
+                <button onClick={() => deleteTaskHandler(_id)}>Delete</button>
+              </div>
+            ))}
+            <input placeholder="Task Name" onChange={e => setTaskName(e.target.value)}/>
+            <button onClick={addTaskHandler}>Add Task</button>
+            <div>
+              <LoginButton />
+            </div> */}
+            Main
+          </main>
+        </> :
+        <Login authenticate={authenticateUser}/>}
+    </ThemeProvider>
     
   );
 }
