1bc9deced45b6f1d8c2a98b35970e6c9fdf50c7a
trunglam7 <trunglam107@gmail.com>
2023-10-04 16:25:19 -0700

    added swipe animation

--- app/components/DogCard.tsx
+++ app/components/DogCard.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import styles from '../styles/dogcard.module.css'
 import Image from 'next/image'
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag, useGesture } from '@use-gesture/react'
 
 export default function DogCard() {
+
+  const [springs, api] = useSpring(() => ({
+    from: { x: 0 },
+  }))
+
+  const bind = useDrag(({ down, movement: [mx] }) => {
+    api.start({ x: down ? mx : 0})
+  })
+
   return (
-    <div className={styles.dog_card_container}>
-        <Image src='/koji.jpg' alt='dog image' fill objectFit='cover'/>
+    <animated.div {...bind()} style={{...springs, touchAction: 'none'}} className={styles.dog_card_container}>
+        <Image src='/koji.jpg' alt='dog image' fill objectFit='cover' style={{pointerEvents: 'none'}}/>
         <b className={styles.dog_name}>KOJI</b>
-    </div>
+    </animated.div>
   )
 }
