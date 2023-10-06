0eb5cee64880412f88e83fc39bb0ea7306bfd795
trunglam7 <trunglam107@gmail.com>
2023-10-05 21:03:10 -0700

    add support multiple dogs

--- app/components/DogCard.tsx
+++ app/components/DogCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/dogcard.module.css'
 import Image from 'next/image'
 import { useSpring, animated } from '@react-spring/web'
@@ -23,7 +23,7 @@ export default function DogCard({name, img, swipe} : DogCardProps) {
 
     const rotation = mx > rotateThreshold ? rotateDeg : mx < (-1 * rotateThreshold) ? (-1 * rotateDeg) : 0;
 
-    if(!down) {
+    if(!down && (mx > rotateThreshold || mx < (-1 * rotateThreshold))) {
       api.start({
         x: mx * 2,
         opacity: 0
@@ -36,17 +36,14 @@ export default function DogCard({name, img, swipe} : DogCardProps) {
           x: 0,
           rotate: 0
         });
-        setTimeout(() => {
-          api.start({
-            opacity: 1
-          });
-          swipe(0, rotateThreshold);
-        }, 500);
 
       }, 500);
-
-
-    } else {
+	}
+	else if (!down && (mx < rotateThreshold && mx > (-1 * rotateThreshold))) {
+		api.start({
+			x: 0
+		})
+    } else if(down){
       api.start({
         x: mx,
         rotate: rotation
@@ -56,7 +53,7 @@ export default function DogCard({name, img, swipe} : DogCardProps) {
 
   return (
     <animated.div {...bind()} style={{...springs, touchAction: 'none', cursor: 'pointer'}} className={styles.dog_card_container}>
-        <Image loader={() => img} src={img} alt='dog image' fill objectFit='cover' style={{pointerEvents: 'none'}}/>
+        <Image loader={() => img} unoptimized src={img} alt='dog image' fill objectFit='cover' style={{pointerEvents: 'none'}}/>
         <b className={styles.dog_name}>{name?.toUpperCase()}</b>
     </animated.div>
   )
--- app/components/VotingPlatform.tsx
+++ app/components/VotingPlatform.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/votingplatform.module.css'
 import DogCard from './DogCard'
 import NotInterestedIcon from '@mui/icons-material/NotInterested';
@@ -12,6 +12,9 @@ export default function VotingPlatform() {
     const dogs = useQuery(api.dogs.getDogs);
 
     const [swipeDirection, setSwipeDirection] = useState(0);
+    const [score, setScore] = useState(0);
+    const [currDog, setCurrDog] = useState(0);
+    const [end, setEnd] = useState(false);
 
     const leftVotingBtnSx = {
         borderRadius: '2rem',
@@ -20,6 +23,13 @@ export default function VotingPlatform() {
             bgcolor: 'coral',
             borderColor: 'coral'
         }
+    }
+
+    const leftVotingBtnActiveSx = {
+        borderRadius: '2rem',
+        color: 'red',
+        bgcolor: 'coral',
+        borderColor: 'coral'
     }
 
     const rightVotingBtnSx = {
@@ -31,29 +41,58 @@ export default function VotingPlatform() {
         }
     }
 
+    const rightVotingBtnActiveSx = {
+        borderRadius: '2rem',
+        color: 'green',
+        bgcolor: 'aquamarine',
+        borderColor: 'aquamarine'
+    }
+
     const handleSwipeDirection = (direction : number, threshold: number) => {
         direction > threshold ? setSwipeDirection(1) : direction < (-1 * threshold) ? setSwipeDirection(-1): setSwipeDirection(0);
     }
+    
+    useEffect(() => {
+        const currScore = score;
+        const dogsLength = dogs?.length ? dogs.length : 0;
+        if(swipeDirection === -1) {
+            setScore(currScore - 1);
 
-
-    const swipeScore = () => {
-        if(swipeDirection === 1) {
-            return 'lime';
-        } else if (swipeDirection === -1) {
-            return 'red';
-        } else {
-            return 'initial';
+            setTimeout(() => {
+                if(currDog + 1 < dogsLength) {
+                    setCurrDog(currDog + 1);
+                } else {
+                    setEnd(true);
+                }
+                setSwipeDirection(0);
+            }, 500)
         }
-    }
+        else if(swipeDirection === 1) {
+            setScore(currScore + 1);
+            
+            setTimeout(() => {
+                if(currDog + 1 < dogsLength) {
+                    setCurrDog(currDog + 1);
+                } else {
+                    setEnd(true);
+                }
+                setSwipeDirection(0);
+            }, 500)
+        }
+    }, [swipeDirection]);
 
     return (
         <div className={styles.voting_platform_container}>
-            {dogs?.map(dog => <DogCard name={dog.name} img={dog.img} swipe={handleSwipeDirection}/>)}
+            {(dogs && !end) ? 
+                <DogCard key={dogs[currDog]._id} name={dogs[currDog].name} img={dogs[currDog].img} swipe={handleSwipeDirection}/>
+                : <p>No More Dogs</p>
+            
+            }
             <div className={styles.vote_btn_container}>
-                <Button sx={leftVotingBtnSx} variant='outlined' disabled={swipeDirection !== 0}>
+                <Button sx={swipeDirection === -1 ? leftVotingBtnActiveSx : leftVotingBtnSx} disabled={end} variant='outlined'>
                     <NotInterestedIcon sx={{fontSize: '2rem'}}/>
                 </Button>
-                <Button sx={rightVotingBtnSx} variant='outlined' disabled={swipeDirection !== 0}>
+                <Button sx={swipeDirection === 1 ? rightVotingBtnActiveSx : rightVotingBtnSx} disabled={end} variant='outlined'>
                     <FavoriteIcon sx={{fontSize: '2rem'}}/>
                 </Button>   
             </div>
